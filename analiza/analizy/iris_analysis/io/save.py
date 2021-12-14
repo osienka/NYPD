dbcbb3cef1cafc313325ae58f2dae905999e7dc0
osienka <92636205+osienka@users.noreply.github.com>
2021-12-14 15:41:48 +0100

    Delete analiza/analizy directory

--- analiza/analizy/iris_analysis/io/save.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-def save(data, fileDir: str):
-    with open(fileDir, 'w', encoding='UTF8', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-    return writer
