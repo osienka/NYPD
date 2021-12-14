3004c05f65da22300f805ab7b2f3cb04c53a0a09
osienka <92636205+osienka@users.noreply.github.com>
2021-12-14 15:47:14 +0100

    Delete run_analysis.py

--- run_analysis.py
+++
@@ -1,9 +0,0 @@
-from iris_analysis import calculate as c
-from iris_analysis.io import load as l
-from iris_analysis.io import save as s
-import sys
-
-def main():
-    s.save(c.calculate(l.load(sys.argv[1])), sys.argv[2])
-
-main()
