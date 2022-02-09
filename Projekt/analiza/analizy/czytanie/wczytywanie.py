cfc9efaeff3cc88728ed1cbfe5b675e82f169e8d
osienka <92636205+osienka@users.noreply.github.com>
2022-02-09 17:01:15 +0100

    Update wczytywanie.py

--- Projekt/analiza/analizy/czytanie/wczytywanie.py
+++ Projekt/analiza/analizy/czytanie/wczytywanie.py
@@ -84,7 +84,7 @@ def lacz(jst: pd.DataFrame, lud: pd.DataFrame):
         jst = code_jst(jst)
         tabela = pd.merge(left=jst, right=lud, on=["code"], how="inner")
     else:
-        lud["nazwa"] = [lud[0][i].lower() if i < 16 else i for i in range(len(lud))]
+        lud["nazwa"] = [lud[i][0].lower() if i < 16 else i for i in range(len(lud))]
         jst["nazwa"] = jst[[4]]
         tabela = pd.merge(left=jst, right=lud, on=["nazwa"], how="inner")
     return tabela
